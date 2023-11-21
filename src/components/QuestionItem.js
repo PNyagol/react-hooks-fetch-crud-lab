7a37f5ba04385bd9f45013a8efeb73c5741561c8
Nyagol <nyagolpeter@gmail.com>
2023-11-22 00:13:41 +0300

    completed

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -9,6 +9,14 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  const handleDelete = () => {fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then(() => {
+      });
+  };
+
   return (
     <li>
       <h4>Question {id}</h4>
@@ -17,7 +25,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
